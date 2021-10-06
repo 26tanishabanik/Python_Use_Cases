ccf29b1e2461e0f5ed26c282ec1e0529f2793e6b
Tanisha Banik <26tanishabanik@gmail.com>
2021-10-06 23:04:29 +0530

    Update auto-detect-DateTime.py

--- auto-detect-date-datetime/auto-detect-DateTime.py
+++ auto-detect-date-datetime/auto-detect-DateTime.py
@@ -1,8 +1,11 @@
 from pandas.api.types import is_datetime64_any_dtype as is_datetime
+
+
 def detect_datetime(path_to_filename):
     df = pd.read_csv(path_to_filename)
     columns = []
     for column in df.columns:
-        if is_datetime(df[column])):
+        if is_datetime(df[column]):
             columns.append(column)
-    return columns
+            
+    return columns 
