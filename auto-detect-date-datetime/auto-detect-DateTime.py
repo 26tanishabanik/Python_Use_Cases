ca19d48f973d794ac52a88142be11d86df01e379
Tanisha Banik <26tanishabanik@gmail.com>
2021-10-07 16:48:37 +0530

    Added a script to detect datetime/date columns

--- auto-detect-date-datetime/auto-detect-DateTime.py
+++ auto-detect-date-datetime/auto-detect-DateTime.py
@@ -1,11 +1,29 @@
-from pandas.api.types import is_datetime64_any_dtype as is_datetime
-
+import pandas as pd
+import numpy as np
 
 def detect_datetime(path_to_filename):
     df = pd.read_csv(path_to_filename)
-    columns = []
+    date_columns = []
+    datetime_columns = []
     for column in df.columns:
-        if is_datetime(df[column]):
-            columns.append(column)
-            
-    return columns 
+        if np.issubdtype(df[column].dtype, np.number):
+            print(column + ' is not a datefield')
+
+        try:
+            dt = pd.to_datetime(df[column])
+            if (dt.dt.floor('d') == dt).all():
+                print(column + ' is a pure date field')
+                date_columns.append(column)
+            elif df[column].str.contains(r"^\d{1,2}:\d{2}:\d{2}$").all():
+                print(column + ' is a pure time field')
+            else:
+                print(column + ' is a Datetime field')
+                datetime_columns.append(column)
+        except BaseException:
+            print(column + ' is not a datefield')
+    print("Dates: ", date_columns)
+    print("DateTime: ", datetime_columns)
+
+
+detect_datetime('datetime.csv')
+
